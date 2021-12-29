5a7c65ef78a7b017968f2a5939aea0cbb85f4caa
Gon <gonza_agl@hotmail.com>
2021-12-29 12:31:35 -0300

    Initializing Node.js tutorial 29/12/21
    Node.js tutorial

---
+++ app.js
@@ -0,0 +1,12 @@
+//Node.js tutorial
+//Working with modules
+console.log("Hello World");
+const tutorial = require('./tutorial.js');
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject);
+
+//The events module and eventemitter class
+
+
+
